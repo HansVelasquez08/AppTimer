4d21e0ec325c35b644f005e34e47cb090366c5f5
GodBlessedHans <haseveco@gmail.com>
2025-03-02 17:00:06 -0500

    Agregar archivos de proyecto.

---
+++ Form1.Designer.cs
@@ -0,0 +1,98 @@
+﻿namespace AppTimer
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.btnActivar = new System.Windows.Forms.Button();
+            this.picJordan = new System.Windows.Forms.PictureBox();
+            this.picLebron = new System.Windows.Forms.PictureBox();
+            this.tmrDesplazar = new System.Windows.Forms.Timer(this.components);
+            ((System.ComponentModel.ISupportInitialize)(this.picJordan)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.picLebron)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // btnActivar
+            // 
+            this.btnActivar.Location = new System.Drawing.Point(699, 390);
+            this.btnActivar.Name = "btnActivar";
+            this.btnActivar.Size = new System.Drawing.Size(89, 48);
+            this.btnActivar.TabIndex = 2;
+            this.btnActivar.Text = "Desplazar";
+            this.btnActivar.UseVisualStyleBackColor = true;
+            this.btnActivar.Click += new System.EventHandler(this.btnActivar_Click);
+            // 
+            // picJordan
+            // 
+            this.picJordan.Image = global::AppTimer.Properties.Resources.imgJordan;
+            this.picJordan.Location = new System.Drawing.Point(320, 259);
+            this.picJordan.Name = "picJordan";
+            this.picJordan.Size = new System.Drawing.Size(149, 125);
+            this.picJordan.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.picJordan.TabIndex = 1;
+            this.picJordan.TabStop = false;
+            // 
+            // picLebron
+            // 
+            this.picLebron.Image = global::AppTimer.Properties.Resources.imgLebron;
+            this.picLebron.Location = new System.Drawing.Point(329, 21);
+            this.picLebron.Name = "picLebron";
+            this.picLebron.Size = new System.Drawing.Size(153, 137);
+            this.picLebron.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.picLebron.TabIndex = 0;
+            this.picLebron.TabStop = false;
+            // 
+            // tmrDesplazar
+            // 
+            this.tmrDesplazar.Tick += new System.EventHandler(this.tmrDesplazar_Tick);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.btnActivar);
+            this.Controls.Add(this.picJordan);
+            this.Controls.Add(this.picLebron);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.picJordan)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.picLebron)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox picLebron;
+        private System.Windows.Forms.PictureBox picJordan;
+        private System.Windows.Forms.Button btnActivar;
+        private System.Windows.Forms.Timer tmrDesplazar;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,139 @@
+﻿using AppTimer.Properties;
+using System;
+using System.Drawing;
+using System.Windows.Forms;
+
+namespace AppTimer
+{
+    public partial class Form1 : Form
+    {
+        public abstract class ObjetoMovible
+        {
+            public int X { get; set; }
+            public int Y { get; set; }
+            public int SpeedX { get; set; }
+            public int SpeedY { get; set; }
+            public PictureBox PictureBox { get; set; }
+
+            public ObjetoMovible(int x, int y, int speedX, int speedY, PictureBox pictureBox)
+            {
+                X = x;
+                Y = y;
+                SpeedX = speedX;
+                SpeedY = speedY;
+                PictureBox = pictureBox;
+            }
+
+            // Método para mover el objeto
+            public virtual void Mover(Form form)
+            {
+                X += SpeedX;
+                Y += SpeedY;
+                PictureBox.Location = new Point(X, Y); // Actualizar la posición del PictureBox
+                form.Refresh(); // Forzar la actualización del formulario
+            }
+
+            // Método para verificar colisiones con otro objeto
+            public bool ColisionaCon(ObjetoMovible otro)
+            {
+                return PictureBox.Bounds.IntersectsWith(otro.PictureBox.Bounds);
+            }
+        }
+
+        public class Lebron : ObjetoMovible
+        {
+            public Lebron(int x, int y, int speedX, PictureBox pictureBox)
+                : base(x, y, speedX, 0, pictureBox)
+            {
+                pictureBox.Name = "picLebron"; // Asignar un nombre para depuración
+            }
+
+            public override void Mover(Form form)
+            {
+                base.Mover(form);
+
+                // Rebotar en los bordes horizontales
+                if (X <= 0 || X >= form.ClientSize.Width - PictureBox.Width)
+                {
+                    SpeedX = -SpeedX;
+                }
+            }
+        }
+
+        public class Jordan : ObjetoMovible
+        {
+            public Jordan(int x, int y, int speedY, PictureBox pictureBox)
+                : base(x, y, 0, speedY, pictureBox)
+            {
+                pictureBox.Name = "picJordan"; // Asignar un nombre para depuración
+            }
+
+            public override void Mover(Form form)
+            {
+                base.Mover(form);
+
+                // Detener el movimiento si llega a la parte superior
+                if (Y <= 0)
+                {
+                    SpeedY = 0; // Detener el movimiento
+                    form.Controls.Remove(PictureBox);
+                    PictureBox.Dispose();
+                }
+            }
+        }
+
+        private Lebron lebron;
+        private Jordan jordan;
+
+        public Form1()
+        {
+            InitializeComponent();
+
+            // Configurar el Timer
+            tmrDesplazar.Interval = 50; // Intervalo en milisegundos
+
+            // Crear el objeto Lebron
+            PictureBox picLebron = new PictureBox
+            {
+                Image = Resources.imgLebron,
+                SizeMode = PictureBoxSizeMode.AutoSize,
+                Location = new Point(10, 20)
+            };
+            picLebron.BringToFront(); // Asegurar que esté al frente
+
+            lebron = new Lebron(10, 20, 5, picLebron);
+
+            // Crear el objeto Jordan
+            PictureBox picJordan = new PictureBox
+            {
+                Image = Resources.imgJordan,
+                SizeMode = PictureBoxSizeMode.AutoSize,
+                Location = new Point(100, this.ClientSize.Height - 50)
+            };
+            picJordan.BringToFront(); // Asegurar que esté al frente
+
+            jordan = new Jordan(100, this.ClientSize.Height - 50, -5, picJordan);
+        }
+
+        private void btnActivar_Click(object sender, EventArgs e)
+        {
+            tmrDesplazar.Enabled = true; // Activar el Timer
+        }
+
+        private void tmrDesplazar_Tick(object sender, EventArgs e)
+        {
+            lebron.Mover(this);
+            jordan.Mover(this);
+
+            // Verificar colisión entre Lebron y Jordan
+            if (lebron.ColisionaCon(jordan))
+            {
+                this.Controls.Remove(lebron.PictureBox);
+                this.Controls.Remove(jordan.PictureBox);
+                lebron.PictureBox.Dispose();
+                jordan.PictureBox.Dispose();
+                tmrDesplazar.Enabled = false; // Detener el Timer
+            }
+        }
+    }
+}
